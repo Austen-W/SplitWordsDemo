6baed7487cc19afb5aed9a54109036140a0b9ba1
Austen_W <2039735801@qq.com>
2018-08-14 15:20:59 +0800

    demo
    Simple demo to spilt words.  2018-8-14

---
+++ cn/strong/DivideWord.java
@@ -0,0 +1,86 @@
+package cn.strong;
+
+import java.io.*;
+import java.util.*;
+
+public class DivideWord {
+	public String left_to_right_divide(String sentence,int max) throws IOException{
+		List words=new ArrayList();	//用于存放词的集合
+		int i=0;
+		String word="";
+		int maxnum=max;	//保存max的值
+		while(i<sentence.length()){
+			//截取max长度的词，若总长度不足，则截取剩余部分
+			if(i+max<=sentence.length()){
+				word=sentence.substring(i,i+max);
+			}else{
+				word=sentence.substring(i);
+			}
+			//如果截取的word是个词或者当前max值为1，则将word加入到词集合中，并把指针向前移动i个位
+			if(isWord(word)||max==1){
+				words.add(word);
+				i=i+max;
+				max=maxnum;
+			}else{
+				max--;
+			}
+		}
+		//将集合合成一个字符串返回
+		String result="";
+		for(int j=0;j<words.size();j++){
+			result+=words.get(j).toString()+"/";
+		}
+		return result;
+	}
+	
+	public String right_to_left_divide(String sentence,int max) throws IOException{
+		List words=new ArrayList();	//用于存放词的集合
+		int i=sentence.length();
+		String word="";
+		int maxnum=max;	//保存max的值
+		while(i>0){
+			//截取max长度的词，若总长度不足，则截取剩余部分
+			if(i-max>=0){
+				word=sentence.substring(i-max,i);
+			}else{
+				word=sentence.substring(0,i);
+			}
+			
+			//如果截取的word是个词或者当前max值为1，则将word加入到词集合中，并把指针向前移动i个位
+			if(isWord(word)||max==1){
+				words.add(word);
+				i=i-max;
+				max=maxnum;
+			}else{
+				max--;
+			}
+		}
+		//将集合合成一个字符串返回
+		String result="";
+		for(int j=words.size()-1;j>=0;j--){
+			result+=words.get(j).toString()+"/";
+		}
+		return result;
+	}
+	
+	//打开词典word.txt，匹配word，判断word是否是一个词
+	public boolean isWord(String word) throws IOException{
+		boolean isword=false;
+		BufferedReader br=new BufferedReader(new FileReader("D:\\jdk\\java\\SpiltWords\\dictionary.txt"));
+		String text;
+		while((text=br.readLine())!=null){
+			String[]key=text.split(",");
+//			System.out.println(key[0]);
+			if(word.equals(key[0])) {
+				isword=true;
+			}
+		}
+		br.close();
+		return isword;
+	}
+	
+	public static void main(String[]args) throws IOException{
+		
+	}
+}
+
---
+++ servlet/LoginServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.*;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import cn.strong.DivideWord;
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public LoginServlet() {
+    	super();
+        // TODO Auto-generated constructor stub
+    }
+ 
+   
+  //放置用户之间通过直接在浏览器输入地址访问这个servlet  
+    protected void doGet(HttpServletRequest request,  
+            HttpServletResponse response) throws ServletException, IOException {  
+     	String encoding = "UTF-8";// 字符编码
+		request.setCharacterEncoding(encoding);// 请求编码
+		response.setContentType("text/html;charset=" + encoding);// 响应编码
+ 
+		PrintWriter out = response.getWriter();
+		String text = request.getParameter("javaText");
+	//	System.out.println(text);
+		text = URLDecoder.decode(text,"UTF-8");
+	//	System.out.println(text);
+	
+		DivideWord dw=new DivideWord();//调用分词
+		String responseText = dw.right_to_left_divide(text, 4);//最大正向匹配的从右向左
+	//	String responseText = dw.left_to_right_divide(text, 4);//最大正向匹配的从左向右
+		
+		out.println(responseText);
+		out.close();
+
+    }  
+  
+    protected void doPost(HttpServletRequest request,  
+            HttpServletResponse response) throws ServletException, IOException {
+    	
+    	doGet(request, response);
+    }  
+
+    
+}
